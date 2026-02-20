4810786191001af7e4f6c452c90c45612192faca
Sampo Sihvola <kassandranhuuto@proton.me>
2026-02-20 12:20:24 +0200

    Update email-sender.py

--- email-sender.py
+++ email-sender.py
@@ -11,7 +11,7 @@ with open(input_file, mode="r", encoding="utf-8-sig", newline="") as file:
 	for row in reader:
 		topic = row.get("topic")
 		emails = row.get("emails")
-		topics.update({"folder": topic, "emails": emails})
+		topics.update({"topic": topic, "emails": emails})
 		topics_and_addresses.append(topics)
 
 outlook = win32.Dispatch('Outlook.Application')
@@ -21,24 +21,26 @@ for n in topics_and_addresses:
 	mailItem.Subject = f"Email about {n['topic']}"
 	mailItem.BodyFormat = 1
 
-mailItem.HTMLBody = f"""
-	<html>
-		<body>
-			<p>paragraph 1</p>
+	mailItem.HTMLBody = f"""
+		<html>
+			<body>
+				<p>paragraph 1</p>
+	
+				<p><b>{n['topic']}</b></p>
+	
+				<p>paragraph 2</p>
+	
+				<p>paragraph 3<b>bolded text</b></p>
+	
+				<p>signature<br>
+				name<br>
+				title<br>
+				organization</p>
+			</body>
+		</html>
+	"""
 
-			<p><b>{n['folder']}</b></p>
+	mailItem.To = n['emails']
+	# save before sending
+	mailItem.Save()
 
-			<p>paragraph 2</p>
-
-			<p>paragraph 3<b>bolded text</b></p>
-
-			<p>signature<br>
-			name<br>
-			title<br>
-			organization</p>
-		</body>
-	</html>
-"""
-
-mailItem.To = n['emails']
-mailItem.Save()
